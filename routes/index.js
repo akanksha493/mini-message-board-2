dab46b5ce722d9aa53344bcd7bb90f43b5babcd4
akanksha493 <akanksha.ydv.aku@gmail.com>
2024-03-30 18:09:45 +0530

    readme added

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const messageModel = require("../models/messages");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const allRecords = await messageModel.find();
-  res.render('index', { title: 'Message Board', messages: allRecords });
+  if(allRecords == null) res.render('index', { title: 'Message Board', messages: [] });
+  else res.render('index', { title: 'Message Board', messages: allRecords });
 });
 router.post("/new", async function(req,res,next){
   await messageModel.create({user: req.body.user, message: req.body.message, added: new Date()});
